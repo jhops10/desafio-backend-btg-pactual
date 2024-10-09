06d63141c49b30c3604ef4fe488a438929c01577
João Paulo <joaowais@gmail.com>
2024-10-09 13:48:15 -0300

    Criar serviço que calcula o valor de todos os pedidos do cliente

--- src/main/java/com/jhops10/order_ms/service/OrderService.java
+++ src/main/java/com/jhops10/order_ms/service/OrderService.java
@@ -6,20 +6,29 @@ import com.jhops10.order_ms.entity.OrderEntity;
 import com.jhops10.order_ms.entity.OrderItem;
 import com.jhops10.order_ms.listener.dto.OrderCreatedEvent;
 import com.jhops10.order_ms.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final MongoTemplate mongoTemplate;
+
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -40,6 +49,18 @@ public class OrderService {
 
         return orders.map(OrderResponse::fromEntity);
     }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+    }
+
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
         return event.itens()
